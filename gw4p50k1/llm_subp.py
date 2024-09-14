0594750d333527be545e699f4fad6d46638b8b15
NikolayBlagoev <workerthreadllc@gmail.com>
2024-09-15 00:10:41 +0300

    switched to dist train

--- gw4p50k1/llm_subp.py
+++ gw4p50k1/llm_subp.py
@@ -6,7 +6,7 @@ from typing import Callable
 from contextlib import redirect_stdout
 import time
 import torch
-from torch import optim, stack, mean, split, cat, tensor
+from torch import optim, stack, mean, split, cat, tensor,save
 from simplellm.tokenizers import SPTokenizer
 from simplellm.dataloaders import TinyStories
 from simplellm.llama import LLamaClassification,LLamaEmbedding,precompute_freqs_cis,TransformerBlock,RMSNorm
@@ -15,24 +15,32 @@ from torchvision import transforms
 import torch.nn.functional as F
 from torch import cuda
 import traceback
-
+import torch.distributed as dist
+import os
 from torch.nn import CrossEntropyLoss, Linear
 # Messages Exchanged by the processes
 @dataclass
 class Gradients:
-    seq_id: bytes
-    data: bytes
-    split_start: str
-    split_end: str
-    node_id: int
-    deferred: int
+    iteraion: int
+    split_start: int
+    split_end: int
+    to: int
+    tag: int
 
 @dataclass
+class SendGradient:
+    iteration: int
+    start: int
+    end: int
+    to: int
+    tag: int
+@dataclass
 class GetGradients:
-    seq_id: bytes
+    iteration: int
     split_start: str
     split_end: str
-    node_id: int
+    to: int
+    tag: int
 
 @dataclass
 class Start:
@@ -70,22 +78,24 @@ class LLama(torch.nn.Module):
         h = self.norm(h)
         output = self.ln(h).float()
         return output
-def run_p(queue_in: Queue, queue_out: Queue, world_size = 4, node_id: int = 0, 
+def run_p(maind_addr,queue_in: Queue, queue_out: Queue, world_size = 4, node_id: int = 0, 
                     device = "cuda"):
-    
+    os.environ["MASTER_ADDR"] = maind_addr
+    os.environ['MASTER_PORT'] = '2950'
+    dist.init_process_group("gloo", rank=node_id, world_size=world_size)
+
     manual_seed(0)
-    seq_l = 128
+    seq_l = 8
     tkns = SPTokenizer()
     ts = TinyStories(tkns,batch_size = 64 // world_size, seq_l=seq_l)
     net = LLama(tkns.vocab_size,dmodel=256,num_heads=8,multiple_of=256,ctx_size=seq_l,n_layers=16)
-    # If necessary half the params to fit more models
-    # net.half()    
+    
     optimizer = optim.SGD(net.parameters(),lr=4e-3,momentum=0,dampening=0,weight_decay=0,nesterov=False)
     with open(f'log{node_id}.txt', 'a') as file, redirect_stdout(file):
-        loc =  ResNetSubP(queue_in,queue_out,net,optimizer,node_id,world_size,ts,device=device)
+        loc =  SubP(queue_in,queue_out,net,optimizer,node_id,world_size,ts,device=device)
         loc.start()
     
-class ResNetSubP(object):
+class SubP(object):
     def __init__(self,queue_in: Queue, queue_out: Queue, net, optimizer, node_id = 0, world_size = 4, ds = None, lr = 4e-3,
                     device = "cuda") -> None:
         self.net = net
@@ -113,6 +123,9 @@ class ResNetSubP(object):
         self.ds = ds
         self.dl = iter(ds)
         
+        self.future_receives = {}
+        self.recvs = []
+        self.sends = []
         for _ in range(self.node_id):
             next(self.dl)
         self.str_ends = dict()
@@ -153,7 +166,7 @@ class ResNetSubP(object):
                         
                         self.epoch += 1
                         if self.iteration >= 80000:
-                            save(net.state_dict(), f"w4p50k1_{self.node_id}.pth")
+                            save(net.state_dict(), f"gw4p50k1_{self.node_id}.pth")
                             exit()
                         
                         self.dl = iter(self.ds)
@@ -172,30 +185,32 @@ class ResNetSubP(object):
                     with open(f"log_stats_proj_2_{self.node_id}.txt", "a") as log:
                         log.write(f"LOSS:{loss.item()}\n")
                     loss.backward()
-                    self.optimizer.step()
+                    # self.optimizer.step()
                     tmp = []
             
                     for param in self.net.parameters():
-                        
-                        tmp.append(param.data.view(-1))
+                        if param.grad == None:
+                            tmp.append(torch.zeros_like(param.data).view(-1))
+                            
+                        else:
+                            tmp.append(param.grad.data.view(-1))
                     prev_grad = cat(tmp).to("cpu")
                     
                     self.prev_aggregation = prev_grad.clone().detach()
                     self.queue_out.put(Start(b'\x01'))
-                    # TODO: GET GRADIENTS
-                    # self.optimizer.step()
+                    
                     continue
                 elif isinstance(task, Gradients):
-                    with open(f"log_stats_proj_2_{self.node_id}.txt", "a") as log:
-                        log.write(f"ADDING GRADIENT from {task.node_id} {self.iteration} {task.deferred}\n")
-                    ret = pickle.loads(task.data)
-                    if task.deferred == self.iteration:
-                        self.aggregation.append((ret,task.split_start,task.split_end))
+                    
+                    
+                    if task.iteraion == self.iteration:
+                        ret = torch.zeros(task.split_end-task.split_start)
+                        self.recvs.append((dist.irecv(tensor=ret,src = task.to, tag = task.tag),ret,task.split_start,task.split_end))
                         
                     else:
-                        if self.next_gradients.get(task.deferred) == None:
-                            self.next_gradients[task.deferred] = []
-                        self.next_gradients[task.deferred].append((ret,task.split_start,task.split_end))
+                        if self.next_gradients.get(task.iteration) == None:
+                            self.next_gradients[task.iteration] = []
+                        self.next_gradients[task.iteration].append((dist.irecv(tensor=ret,src = task.to, tag = task.tag),ret,task.split_start,task.split_end))
                     continue
                 elif isinstance(task, GetGradients):
                     tmp = self.prev_aggregation.clone().detach()
@@ -209,8 +224,16 @@ class ResNetSubP(object):
                             end = v[1]
                             break
                     tmp = tmp[strt:end]
-                    self.queue_out.put(Gradients(int(strt).to_bytes(4,byteorder = "big") + int(end).to_bytes(4,byteorder = "big") + task.seq_id, pickle.dumps(tmp),"","",self.node_id,self.iteration),True)
+                    self.sends.append(dist.isend(tensor=tmp,dst = task.to, tag = task.tag))
+                    self.queue_out.put(SendGradient(task.iteration,strt,end,task.to,task.tag))
                 elif isinstance(task, Aggregate):
+                    for v in self.sends:
+                        v.wait()
+                    self.sends.clear()
+                    for v in self.recvs:
+                        v[0].wait()
+                        self.aggregation.append((v[1],v[2],v[3]))
+                    self.recvs.clear()
                     self.iteration += 1
                     with open(f"log_stats_proj_2_{self.node_id}.txt", "a") as log:
                         log.write(f"===AGGEGATING==== {len(self.aggregation)}\n")
@@ -222,9 +245,7 @@ class ResNetSubP(object):
                         if tmp.get(k) == None:
                             tmp[k] = ([],self.aggregation[i][2])
                         tmp[k][0].append(self.aggregation[i][0])
-                        # self.aggregation[i][0].data[0:self.aggregation[i][1]] = self.prev_aggregation.data[0:self.aggregation[i][1]]
-                        # .data[self.aggregation[i][2]:] = self.prev_aggregation.data[self.aggregation[i][2]:]
-                        # self.aggregation[i] = self.aggregation[i][0]
+                        
                         i += 1
                     for k,v in tmp.items():
                         strt = k
@@ -273,7 +294,7 @@ class ResNetSubP(object):
                     ret = split(ret, self.len_sizes)
                     
                     for i, param in enumerate(self.net.parameters()):
-                        param.data = ret[i].view(self.sizes[i]).to(self.device)
+                        param.data -= self.lr*ret[i].view(self.sizes[i]).to(self.device)
                     
                     cuda.empty_cache()
         except Exception:
--- w4p50k1/training_protocol.py
+++ w4p50k1/training_protocol.py
@@ -9,15 +9,14 @@ from deccom.protocols.wrappers import *
 from datetime import datetime
 import asyncio
 from traceback import print_exception, format_exc
-from resnet_trainer import *
+from llm_subp import *
 from deccom.cryptofuncs.hash import SHA256
 
     
 
 class TrainingProtocol(AbstractProtocol):
     required_lower = AbstractProtocol.required_lower + \
-        ["find_peer", "set_stream_callback",
-            "open_connection", "send_stream", "get_peer", "get_peers", "connected_callback","disconnected_callback"]
+        ["find_peer", "get_peer", "get_peers", "connected_callback","disconnected_callback"]
     
     GRADIENT = int.from_bytes(b'\x17',byteorder="big")
     def __init__(self, world_size: int, k: int, queue_in: Queue, queue_out: Queue, subprocess:Process, submodule=None, callback: Callable[[tuple[str, int], bytes], None] = lambda : ...):
@@ -25,6 +24,7 @@ class TrainingProtocol(AbstractProtocol):
         super().__init__(submodule, callback)
         self.group = []
         self.k = k
+        self.tag = 0
         self.queue_in = queue_in
         self.queue_out = queue_out
         self.subprocess = subprocess
@@ -42,9 +42,7 @@ class TrainingProtocol(AbstractProtocol):
     @bindto("open_connection")
     async def _lower_open_connection(self, remote_ip, remote_port, node_id: bytes):
         return
-    @bindto("send_stream")
-    async def _lower_send_stream(self, node_id, data, ignore_sz = 0):
-        return
+    
     @bindto("get_peer")
     def _lower_get_peer(self, node_id)->Peer:
         return None
@@ -83,13 +81,12 @@ class TrainingProtocol(AbstractProtocol):
                 return
             task = self.queue_in.get(True)
             try:
-                if isinstance(task,Gradients):
-                    await self.send_stream(task.seq_id[8:], int(task.deferred).to_bytes(4,byteorder="big") + task.seq_id[:8] + task.data)
+                if isinstance(task,SendGradient):
+                    pr = await self._lower_find_peer(SHA256(str(task.to)))
+                    await self.send_datagram(int(task.iteration).to_bytes(4,byteorder="big") + int(task.tag).to_bytes(1,byteorder="big")+int(task.start).to_bytes(8,byteorder="big")+int(task.end).to_bytes(8,byteorder="big")+self.peer.id_node, pr.addr)
                     continue
                 elif isinstance(task,Start):
-                    # Distribute Gradients
-                    # with open(f"log_stats_proj_2_{self.peer.pub_key}.txt", "a") as log:
-                    #     log.write(f"DISTRIBUTING GRADIENTS\n")
+                    
                     for i in range(self.world_size):
                         if i ==  int(self.peer.pub_key):
                             continue
@@ -107,16 +104,20 @@ class TrainingProtocol(AbstractProtocol):
                             log.write(f"TO {pb} {pr.pub_key} goes group {group}\n")
                         
                         if group == 0:
-                            self.queue_out.put(GetGradients(pr.id_node, "transformer_8", "ln",0),True)
+                            self.queue_out.put(GetGradients(self.iteration, "transformer_8", "ln",pb,self.tag),True)
                         elif group == 1:
-                            self.queue_out.put(GetGradients(pr.id_node, "embedding", "transformer_7",0),True)
+                            self.queue_out.put(GetGradients(self.iteration, "embedding", "transformer_7",pb,self.tag),True)
+                            
                         elif group == 2:
-                            await self.send_datagram(int(self.iteration).to_bytes(4,byteorder="big")+self.peer.id_node, pr.addr)
+                            await self.send_datagram(int(self.iteration).to_bytes(4,byteorder="big") + int(self.tag).to_bytes(1,byteorder="big")+int(0).to_bytes(8,byteorder="big")+int(0).to_bytes(8,byteorder="big")+self.peer.id_node, pr.addr)
+                    
+                        self.tag += 1
+                        self.tag = self.tag % 253
                             # self.queue_out.put(GetGradients(pr.id_node, "layer2", "layer2",0),True)
                         # elif group == 3:
                         #     self.queue_out.put(GetGradients(pr.id_node, "layer3", "layer3",0),True)
-                        self.can_acrue = True
-                        self.aggregate()
+                    self.can_acrue = True
+                    self.aggregate()
                     continue
 
                 continue
@@ -156,11 +157,17 @@ class TrainingProtocol(AbstractProtocol):
     
     def process_datagram(self, addr: tuple[str, int], data: bytes):
         itr = int.from_bytes(data[:4],byteorder="big")
-        data = data[4:]
-        nodeid = data
+        tag = data[4]
+        strt = int.from_bytes(data[5:13],byteorder="big")
+        end = int.from_bytes(data[13:21],byteorder="big")
+        nodeid = data[21:]
         pr = self._lower_get_peer(nodeid)
-        with open(f"log_stats_proj_2_{self.peer.pub_key}.txt", "a") as log:
-            log.write(f"NO GRADIENT FROM {pr.pub_key} {itr}\n")
+        if strt == end:
+            with open(f"log_stats_proj_2_{self.peer.pub_key}.txt", "a") as log:
+                log.write(f"NO GRADIENT FROM {pr.pub_key} {itr}\n")
+        else:
+            self.queue_out.put(Gradients(itr,strt,end,int(pr.pub_key),tag), True)
+
         if self.gradients_received.get(nodeid) == None:
             self.gradients_received[nodeid] = dict()
         self.gradients_received[nodeid][itr] = 1
@@ -171,70 +178,13 @@ class TrainingProtocol(AbstractProtocol):
         if cl == self.world_size - 1:
             self.aggregate()
 
-    @bindfrom("stream_callback")
-    def process_data(self, data:bytes, nodeid, addr, retry = False):
-        itr = int.from_bytes(data[:4],byteorder="big")
-        data = data[4:]
-        p = self._lower_get_peer(nodeid)
-        try:
-            if self.gradients_received.get(nodeid) == None:
-                self.gradients_received[nodeid] = dict()
-            
-            self.queue_out.put(Gradients(b'\x01',data[8:],int.from_bytes(data[0:4],byteorder="big"),int.from_bytes(data[4:8],byteorder="big"),p.pub_key,itr), True)
-            
-            self.gradients_received[nodeid][itr] = 1
-            cl = 0
-            for k,v in self.gradients_received.items():
-                if v.get(self.iteration) != None:
-                    cl +=1
-            if cl == self.world_size - 1:
-                self.aggregate()
-            
-        except Exception as e:
-                with open(f'log{self.peer.pub_key}.txt', 'a') as f:
-                    f.write(str(e))
-                    f.write("!!!!!!!!!!!!!!!\n")
-                    f.write(format_exc())
+    
     
       
-    async def send_stream(self, node_id, data):
-        # print("SENDING TO")
-        
-        try:
-                
-            await asyncio.wait_for(self._lower_find_peer(bytes(node_id)), timeout=10)
-            p: Peer = self._lower_get_peer(node_id)
-            if p == None:
-                raise asyncio.TimeoutError()
-                
-        except asyncio.TimeoutError:
-            with open(f"log_stats_proj_2_{self.peer.pub_key}.txt", "a") as log:
-                log.write(f"FAILED TO FIND PEER:{node_id}\n")
-
-            
- 
-        ret = await self._lower_open_connection(p.addr[0], p.tcp, p.id_node, port_listen = 0)
-        if not ret:
-            with open(f"log_stats_proj_2_{self.peer.pub_key}.txt", "a") as log:
-                log.write(f"FAILED TO OPEN CONNECTION:{datetime.now()},{seqdata},{p.pub_key}\n")
-                
-            
-        ret = await self._lower_send_stream(node_id, data)
+    
    
 
-    def get_lowest_stream(self):
-        submodule = self.submodule
-        while submodule != None and not hasattr(submodule, "get_lowest_stream") and hasattr(submodule, "submodule") :
-            submodule = submodule.submodule
-        if submodule != None and hasattr(submodule, "get_lowest_stream"):
-            ret = submodule.get_lowest_stream()
-            if ret == None:
-                return self
-            else:
-                return ret
-        else:
-            
-            return self
+    
 
     
     async def stop(self):
